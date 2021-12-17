899f7305674de4ae46687f3779328671af5e0674
DanLivassan <daniloxc@msn.com>
2021-12-17 18:53:13 -0300

    Implement wait_for_db function

--- app/app/settings.py
+++ app/app/settings.py
@@ -66,10 +66,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME') or 'DB_NAME',
+        'USER': os.environ.get('DB_USER') or 'DB_USER',
+        'PASSWORD': os.environ.get('DB_PASS') or 'DB_PASS',
+        'HOST': os.environ.get('DB_HOST') or 'DB_HOST',
     }
 }
 
